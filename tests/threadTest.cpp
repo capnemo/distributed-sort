73f83a0c10a28a7f4a5adf611f9ac04aefeea0ae
Chandra Annamaneni <chandra627@gmail.com>
2017-12-02 07:17:05 -0800

    API v2

--- tests/threadTest.cpp
+++ tests/threadTest.cpp
@@ -7,24 +7,27 @@
 int genRand(const strVec& args);
 int main(int argc, char *argv[])
 {
-    threadPool tP(4);
-    tP.startWorkers();
+    funcMap tab = {{'r', genRand}};
+    threadPool tP(4, tab);
     std::vector<struct subTask> rVec;
     std::string segSize = std::to_string(100000);
     std::string fileStub = "/tmp/out.";
 
     for (uint32_t i = 0; i < 12; i++) {
-        struct subTask s = {i, genRand, {segSize, 
-                                         fileStub + std::to_string(i)}, 10};
-        tP.addToPool(s);
+        std::string id;
+        tP.dispatchTask('r', {segSize, fileStub + std::to_string(i)}, id);
     }
 
-    while(rVec.size() < 12)
-        tP.getTaskResults(rVec);
+    tP.startDispatch();
+    strVec fails;
+    tP.waitForCompletion(fails);
+    if (fails.size() != 0) {
+        std::cout << "Error!"  << std::endl;
+        for (auto mem:fails)
+            std::cout << mem << std::endl;
+    }
 
-    tP.stopAllThreads();
-   //strVec fArgs = {"100", "/tmp/out"};
-   //genRand(fArgs);
+    tP.terminate();
 }
 
 
