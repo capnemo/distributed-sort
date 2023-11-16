2203b3cdcb5813d45cd0d9b40326b032b1157136
Chandra Annamaneni <chandra627@gmail.com>
2023-11-15 17:25:22 -0800

    Data file generation utility.

---
+++ data_gen/gen_lines.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+import sys
+import random
+import threading
+import time
+
+from os import cpu_count
+from multiprocessing import Process, Lock
+
+def gen_lines(line_len, num_lines, file_ob, lck):
+    bf=[]
+    for nl in range(0, num_lines):
+        ln=""
+        for rc in range(0, line_len):
+            ln += chr(random.randrange(ord('a'), ord('z')))
+        bf.append(ln)
+        if (len(bf) == 1000):
+            write_to_file(file_ob, bf, lck)
+            bf = []
+
+    write_to_file(file_ob, bf, lck)
+    print("thread ends")
+
+def write_to_file(file_object, lines, lck):
+    lck.acquire();
+    for l in lines:
+        file_object.write(l + '\n');
+    file_object.flush()
+    lck.release();
+
+if __name__ == "__main__":
+    if (len(sys.argv) != 4):
+        print("error")
+        sys.exit()
+
+    line_len = int(sys.argv[1])
+    num_recs = int(sys.argv[2])
+    file_name = sys.argv[3]
+
+    num_threads = int(cpu_count()/2)
+    recs_per_thread = int(num_recs/num_threads)
+    outfile = open(file_name, 'w')
+    lk = threading.Lock();
+    for t in range(0, num_threads):
+        Process(target=gen_lines, args=(line_len, recs_per_thread, 
+                outfile, lk)).start()
+        print("starting thread " + str(t))
+
+    outfile.close()
